adddfc6eee3f1c29984805f83227a8a78a0e35dd
Dynex [DNX] <113523376+dynexcoin@users.noreply.github.com>
2023-09-25 13:40:52 +0200

    Add files via upload

--- dynexsdk/qiskit/tools/_compiler.py
+++ dynexsdk/qiskit/tools/_compiler.py
@@ -54,7 +54,9 @@ def _dags_2_qobj_parallel(dag, config=None, basis_gates=None, coupling_map=None)
 def execute(circuit, backend = None,
             config=None, basis_gates=None, coupling_map=None, initial_layout=None,
             shots=1024, max_credits=10, seed=None, qobj_id=None, hpc=None,
-            skip_transpiler=False, seed_mapper=None):
+            skip_transpiler=False, seed_mapper=None,
+            sampler = 'sa', num_reads = 5000, annealing_time = 300, mainnet=False, logging=False, 
+            constrain_measured_qubits=True, constraint_strength = 10):
 
     outputs = list()
     inputs = list()
@@ -62,146 +64,122 @@ def execute(circuit, backend = None,
     qubit_biases = circuit.annealergraph.qubitbiases
     coupler_strengths = circuit.annealergraph.couplerstrengths
 
-    print("\n[DYNEX] Embedding has been built:")
+    print('[DYNEX] Qiskit circuit qubits:',len(circuit.annealergraph.qubits));
+    print("[DYNEX] Embedding has been built:")
     print("\tnumber of qubits: ", len(qubit_biases))
     print("\tnumber of couplers: ", len(coupler_strengths))
-    print("\tnumber of non-gate couplers: {}\n".format(len(coupler_strengths)-circuit.annealergraph.numgatecouplers))
+    print("\tnumber of non-gate couplers: {}".format(len(coupler_strengths)-circuit.annealergraph.numgatecouplers))
 
     qubits = [];
+    qubits_in = [];
+    qubits_out = [];
+    
 
+    # measured qubits:
     for qubit in circuit.annealergraph.qubits.keys():
         if isinstance(circuit.annealergraph.qubits[qubit], dict):
             if circuit.annealergraph.qubits[qubit]['measured'] == True:
                 outputs.append(circuit.annealergraph.qubits[qubit]['components'][-1])
+                qubits_out.append(qubit);
                 # can't omit output bit from being included as input, but is its put
                 # first in the list, it will be zero the top portion of the readout
-                ans = input("Constrain input of measured qubit {} to be 0 (y/n)? ".format(qubit))
+                if constrain_measured_qubits:
+                	ans = 'y'
+                	print('[DYNEX] INFO:',qubit,'was constrained to be 0 (contrain_measured_qubits=True)');
+                else:
+                	ans = input("Constrain input of measured qubit {} to be 0 (y/n)? ".format(qubit))
                 if ans == 'y':
                     change = circuit.annealergraph.qubits[qubit]['components'][0]
-                    circuit.annealergraph.qubitbiases[change] = circuit.annealergraph.qubitbiases[change] + 5
+                    circuit.annealergraph.qubitbiases[change] = circuit.annealergraph.qubitbiases[change] + constraint_strength
                 else:
                     inputs.append(circuit.annealergraph.qubits[qubit]['components'][0])
+                    qubits_in.append(qubit);
                 qubits.append(qubit);
 
+    # unmeasured qubits:
     for qubit in circuit.annealergraph.qubits.keys():
         if isinstance(circuit.annealergraph.qubits[qubit], dict):
             if circuit.annealergraph.qubits[qubit]['measured'] == False:
-                ans = input("Constrain input of unmeasured qubit {} to be 0 (y/n)? ".format(qubit))
+                if constrain_measured_qubits:
+                	ans = 'n'
+                else:
+                	ans = input("Constrain input of unmeasured qubit {} to be 0 (y/n)? ".format(qubit))
                 if ans == 'y':
                     change = circuit.annealergraph.qubits[qubit]['components'][0]
-                    circuit.annealergraph.qubitbiases[change] = circuit.annealergraph.qubitbiases[change] + 5
+                    circuit.annealergraph.qubitbiases[change] = circuit.annealergraph.qubitbiases[change] + constraint_strength
                 else:
                     inputs.append(circuit.annealergraph.qubits[qubit]['components'][0])
+                    qubits_in.append(qubit);
                 qubits.append(qubit);
             
     circuit.annealergraph.print_chimera_graph_to_file()
  
-    samp = input("\nHow many samples (=num_reads)? ")
-    samp = int(samp)
-
-    steps = input("\nHow many ODE integration steps (=annealing_time)? ")
-    steps = int(samp)
-
     if 'sim' in sys.argv:
-        try:
-            import dimod
-            from dwave.system.samplers import DWaveSampler
-            from dwave.cloud.exceptions import SolverOfflineError
-            from dwave.system.composites import EmbeddingComposite
-            import minorminer
-        except:
-            print('Dimod not installed - cannot run or simulate generated embedding. Install dimod with pip install dimod')
-            return
-
-
-        ans = 'y'
-        if len(circuit.annealergraph.qubitbiases) > 19:
-            ans = input("WARNING: Embedding uses {} qubits - ExactSolver simulation could take way too long or cause your computer to crash. Type 'y' to continue: ".format(len(circuit.annealergraph.qubitbiases)))
-        
-        if ans == 'y' or ans == 'Y':
-            print("Simulating problem with {} qubits".format(len(circuit.annealergraph.qubitbiases)))
-            qubit_biases = circuit.annealergraph.qubitbiases
-            coupler_strengths = circuit.annealergraph.couplerstrengths
-        
-            print("\nQubit Biases:")
-            for key in qubit_biases.keys():
-                print(key, "\t", qubit_biases[key])
-            print("\nCoupler Strengths:")
-            for key in coupler_strengths.keys():
-                print(key, "\t", coupler_strengths[key])
-            print("\n")
-
-            bqm = dimod.BinaryQuadraticModel(qubit_biases, coupler_strengths, 0, dimod.BINARY)
-            sampler = dimod.ExactSolver()
-    
-            response = sampler.sample(bqm)
-
-            groundstate = 1000000
-            for sample, energy in response.data(['sample','energy']):
-                if energy<groundstate:
-                    groundstate = round(energy,1)
-        
-            print("Ground State: ", groundstate)
-            function = list()
-            for sample, energy in response.data(['sample', 'energy']):
-                if round(energy,1) == groundstate:
-                    print(sample, round(energy,1))
-                    row = []
-                    for inp in inputs:
-                        row.append(sample[inp])
-                    for outp in outputs:
-                        row.append(sample[outp])
-                    function.append(row)
-            print('\n')
-
-            function.sort()
-            for i in range(len(function)):
-                print(function[i])
-        else:
-            print('Quitting.')
-            
+    	# simulated:
+    	print('not implemented');
     else:
         try:
             import dimod
             import dynex
+            from dimod import SimulatedAnnealingSampler
         except:
             print('Dynex SDK not installed - cannot run generated embeddings. Please install and configure the Dynex SDK with pip install dynex.')
             return
 
         bqm = dimod.BinaryQuadraticModel(qubit_biases, coupler_strengths, 0, dimod.BINARY)
-        print('\nQubit biases:\n',qubit_biases)
-        print('\nCoupler strength:\n',coupler_strengths)
-        print('\n#bqm variables:\n',len(bqm.variables))
+        #print('\nQubit biases:\n',qubit_biases)
+        #print('\nCoupler strength:\n',coupler_strengths)
+        print('[ÐYNEX] BQM gnerated');
+        print('\tnumber of variables:',len(bqm.variables));
+        print('\tnumber of qubit biases:',len(qubit_biases));
+        print('\tnumber of coupler_strengths:',len(coupler_strengths));
+
         kwargs = {}
-        print("\nRunning Qiskit Circuit on the Dynex Neuromorphic Platform...")
-        model = dynex.BQM(bqm, logging=True);
-        sampler = dynex.DynexSampler(model,  mainnet=True, description='IBM Qiskit', logging=True);
-        response = sampler.sample(num_reads=samp, annealing_time = steps, debugging=False);
-        print("Done.")
+        
+        if sampler == 'dynex':
+        	print("[DYNEX] Running Qiskit Circuit on the Dynex Neuromorphic Platform...")
+        	model = dynex.BQM(bqm, logging=logging);
+        	sampler = dynex.DynexSampler(model,  mainnet=mainnet, description='IBM Qiskit', logging=logging);
+        	response = sampler.sample(num_reads=num_reads, annealing_time = annealing_time, debugging=False);
+        else:        
+        	print("[DYNEX] Running Qiskit Circuit with Simulated Annealing Sampler...")
+        	sampler = SimulatedAnnealingSampler()
+        	response = sampler.sample(bqm, num_reads=10)
+        
+        print("[DYNEX] Sampling completed. Result:")
         print(response)
 
-        groundstate = 1000000
-        for sample, energy in response.data(['sample','energy']):
-            if energy<groundstate:
-                groundstate = round(energy,1)
-        print("Ground State: ", groundstate)
+        groundstate = round(response.first.energy, 1);
+        print("[DYNEX] Found ground state (energy): ", groundstate)
 
         function = list()
         for sample, energy in response.data(['sample', 'energy']):
             if round(energy,1) == groundstate:
-                #print(sample, round(energy,1))
                 row = []
                 for inp in inputs:
                     row.append(sample[inp])
                 for outp in outputs:
                     row.append(sample[outp])
                 function.append(row)
-        print('\n')
-    
+        
         # output qubit reads:
-        print('qubits:')
-        print(qubits);
-        print('reads:')
         function.sort()
         for i in range(len(function)):
-            print(function[i])
+            print('[DYNEX]',function[i])
+        
+        # generate return values:
+        best_sample = response.first.sample;
+        qubits_measured = {};
+        qubits_unmeasured = {};
+        for q in range(0, len(qubits_in)):
+        	qubits_unmeasured[qubits_in[q]] = best_sample[inputs[q]];
+        for q in range(0, len(qubits_out)):
+        	qubits_measured[qubits_out[q]] = best_sample[outputs[q]];
+        
+        # return qubit reads:
+        return qubits_measured, qubits_unmeasured
+            
+            
+            
+            
+            
